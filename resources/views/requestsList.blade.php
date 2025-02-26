6b55d56f2d132e29483b7470edd980fbdf278423
WissamDouskary <wissamdouskary99@gmail.com>
2025-02-26 12:11:14 +0000

    create success models

--- resources/views/requestsList.blade.php
+++ resources/views/requestsList.blade.php
@@ -5,6 +5,33 @@
         </h2>
     </x-slot>
 
+    @if(session('success'))
+    <div id="success-message" class="bg-green-100 border-l-4 border-green-500 text-green-700 p-4 mb-4 rounded-md shadow-md relative">
+        <div class="flex items-center">
+            <svg class="h-6 w-6 text-green-500 mr-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v4a1 1 0 002 0V7zM9 12a1 1 0 012 0v1a1 1 0 01-2 0v-1z" clip-rule="evenodd" />
+            </svg>
+            <p class="font-semibold">{{ session('success') }}</p>
+        </div>
+        <!-- Close Button -->
+        <button id="close-button" class="absolute top-2 right-2 text-green-700 hover:text-green-900">
+            <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path fill-rule="evenodd" d="M6.293 4.293a1 1 0 011.414 0L10 6.586l2.293-2.293a1 1 0 111.414 1.414L11.414 8l2.293 2.293a1 1 0 01-1.414 1.414L10 9.414l-2.293 2.293a1 1 0 01-1.414-1.414L8.586 8 6.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
+            </svg>
+        </button>
+    </div>
+
+    <script>
+        setTimeout(function() {
+            document.getElementById('success-message').style.display = 'none';
+        }, 3000);
+
+        document.getElementById('close-button').addEventListener('click', function() {
+            document.getElementById('success-message').style.display = 'none';
+        });
+    </script>
+    @endif
+
     <!-- User List -->
     <ul class="space-y-6 px-4 mt-6">
         @unless (count($receivedRequests) != 0)
